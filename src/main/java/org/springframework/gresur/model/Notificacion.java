936d8fbad0fcca6cea8919dd29fa9c90601a0faa
Borja <borjar20@gmail.com>
2020-11-13 02:11:08 +0100

    Notificacion

---
+++ src/main/java/org/springframework/gresur/model/Notificacion.java
@@ -0,0 +1,48 @@
+package org.springframework.gresur.model;
+
+import java.sql.Blob;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.Lob;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name="notificaciones")
+public class Notificacion extends BaseEntity{
+
+	@NotNull
+	@Enumerated(value = EnumType.STRING)
+	@Column(name ="tipo_notificacion")
+	private TipoNotificacion tipoNotificacion;
+	
+	@NotBlank
+	@Lob
+	private Blob cuerpo;
+	
+	@NotNull
+	private Boolean leido;
+	
+	@ManyToOne(optional = true)
+	private Personal emisor;
+	
+	@ManyToMany
+	@JoinTable(
+			name ="enviado_a",
+			joinColumns = @JoinColumn(name = "notificacion_id"),
+			inverseJoinColumns = @JoinColumn(name = "personal_id"))
+	private List<Personal> receptores;
+	
+}
---
+++ src/main/java/org/springframework/gresur/model/TipoNotificacion.java
@@ -0,0 +1,5 @@
+package org.springframework.gresur.model;
+
+public enum TipoNotificacion {
+	URGENTE,NORMAL,SISTEMA
+}
