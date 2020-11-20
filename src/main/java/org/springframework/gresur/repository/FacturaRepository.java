125dad1cba670b8fdb86bf55b5af3fb40b25682a
alesancor1 <alesancor1@alum.us.es>
2020-11-20 22:11:43 +0100

    Bugfix en la implementacion de la demanda

--- src/main/java/org/springframework/gresur/repository/FacturaRepository.java
+++ src/main/java/org/springframework/gresur/repository/FacturaRepository.java
@@ -4,9 +4,7 @@ import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.gresur.model.Factura;
-import org.springframework.gresur.model.LineaFactura;
 
-public interface FacturaRepository<T extends Factura> extends CrudRepository<T, Long> {
-	
-	public List<LineaFactura> findAllLineasFactura();
+public interface FacturaRepository<T extends Factura> extends CrudRepository<T, Long> {	
+	public List<T> findAll();
 }
