9e35b38c62a96ce78eac31ac14a2bd39caaa8d02
alesancor1 <alesancor1@alum.us.es>
2020-11-20 19:00:36 +0100

    Implementación de la propiedad derivada "demanda" en el ProductoService

--- src/main/java/org/springframework/gresur/service/ProductoService.java
+++ src/main/java/org/springframework/gresur/service/ProductoService.java
@@ -1,8 +1,12 @@
 package org.springframework.gresur.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
+import org.springframework.gresur.model.LineaFactura;
 import org.springframework.gresur.model.Producto;
+import org.springframework.gresur.repository.FacturaEmitidaRepository;
 import org.springframework.gresur.repository.ProductoRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,10 +15,14 @@ import org.springframework.transaction.annotation.Transactional;
 public class ProductoService {
 	
 	private ProductoRepository productoRepository;
+	private FacturaEmitidaRepository facturaRepository;
+	
+	/* CRUD METHODS */
 	
 	@Autowired
-	public ProductoService(ProductoRepository productoRepository) {
+	public ProductoService(ProductoRepository productoRepository, FacturaEmitidaRepository facturaRepository) {
 		this.productoRepository = productoRepository;
+		this.facturaRepository = facturaRepository;
 	}
 	
 	@Transactional(readOnly = true)
@@ -35,5 +43,20 @@ public class ProductoService {
 	@Transactional
 	public void deleteById(Long id) throws DataAccessException{
 		productoRepository.deleteById(id);
-	} 
+	}
+	
+	/* USER STORIES */
+	
+	@Transactional(readOnly = true)
+	public Double getDemanda(Producto p) throws DataAccessException{
+		List<LineaFactura> lf = facturaRepository.findAllLineasFactura();
+		Long totalVentas = lf.stream()
+							 .mapToLong(x->x.getCantidad())
+							 .sum();
+		Double ventasProducto = lf.stream()
+								  .filter(x->x.getProducto().equals(p))
+								  .mapToDouble(x->x.getCantidad())
+								  .sum();
+		return ventasProducto/totalVentas;
+	}
 }
