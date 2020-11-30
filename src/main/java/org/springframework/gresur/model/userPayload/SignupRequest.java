707fb094e1f07f5c592fdd7d1078617a3b16a077
Borja <borjar20@gmail.com>
2020-11-30 15:30:56 +0100

    Sol error signup

--- src/main/java/org/springframework/gresur/model/userPayload/SignupRequest.java
+++ src/main/java/org/springframework/gresur/model/userPayload/SignupRequest.java
@@ -14,11 +14,6 @@ public class SignupRequest {
 	@NotBlank
     @Size(min = 3, max = 20)
     private String username;
- 
-    @NotBlank
-    @Size(max = 50)
-    @Email
-    private String email;
     
     private Set<String> roles;
     
