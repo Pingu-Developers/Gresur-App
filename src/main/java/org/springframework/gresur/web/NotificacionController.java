16ef8da3eefbb3c83a330da6ff3d6b1ac41acc5e
Borja <borjar20@gmail.com>
2021-01-02 20:09:39 +0100

    NotificationCenter v1.0

--- src/main/java/org/springframework/gresur/web/NotificacionController.java
+++ src/main/java/org/springframework/gresur/web/NotificacionController.java
@@ -1,17 +1,23 @@
 package org.springframework.gresur.web;
 
 import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.gresur.configuration.services.UserDetailsImpl;
 import org.springframework.gresur.model.LineaEnviado;
 import org.springframework.gresur.model.Notificacion;
 import org.springframework.gresur.model.Personal;
-import org.springframework.gresur.model.userPayload.JwtResponse;
+import org.springframework.gresur.model.TipoNotificacion;
 import org.springframework.gresur.model.userPayload.MessageResponse;
 import org.springframework.gresur.repository.UserRepository;
+import org.springframework.gresur.service.AdministradorService;
 import org.springframework.gresur.service.LineasEnviadoService;
 import org.springframework.gresur.service.NotificacionService;
+import org.springframework.gresur.service.PersonalService;
+import org.springframework.gresur.util.Tuple3;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -19,6 +25,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -35,6 +42,9 @@ public class NotificacionController {
 	
 	@Autowired
 	LineasEnviadoService lineaEnviadoService;
+	
+	@Autowired
+	AdministradorService admService;
 	
 	@Autowired
 	public NotificacionController(NotificacionService notificacionService) {
@@ -76,5 +86,31 @@ public class NotificacionController {
 		else {
 			return ResponseEntity.badRequest().body(new MessageResponse("Error: Notification not found!"));
 		}
+	}
+
+	
+	@PostMapping()
+	public ResponseEntity<?> nuevaNotificacion(@Valid @RequestBody Tuple3<List<Long>,String,String> noti) {
+		
+		Authentication user = SecurityContextHolder.getContext().getAuthentication();
+		UserDetailsImpl userDetails = (UserDetailsImpl) user.getPrincipal();
+		
+		Personal per = userRepository.findByUsername(userDetails.getUsername()).orElse(null).getPersonal();
+		
+		List<Personal> receptores = noti.getE1().stream().map(x -> admService.findPersonal(x)).collect(Collectors.toList());
+		
+		Notificacion nuevaNoti = new Notificacion();
+		
+		nuevaNoti.setEmisor(per);
+		nuevaNoti.setCuerpo(noti.getE2());
+		nuevaNoti.setTipoNotificacion(TipoNotificacion.valueOf(noti.getE3()));
+		
+		try {
+			Notificacion res = notificacionService.save(nuevaNoti, receptores);
+			return ResponseEntity.ok(res);
+		} catch (Exception e) {
+			return ResponseEntity.badRequest().body(e);
+		}
+		
 	}
 }
