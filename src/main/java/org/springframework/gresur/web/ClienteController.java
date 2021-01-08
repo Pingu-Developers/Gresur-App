cca8bec64bed8594a8720f78c3c89a4e55524a11
alesancor1 <alesancor1@alum.us.es>
2021-01-08 22:01:46 +0100

    Nuevo pedido submit

--- src/main/java/org/springframework/gresur/web/ClienteController.java
+++ src/main/java/org/springframework/gresur/web/ClienteController.java
@@ -1,14 +1,18 @@
 package org.springframework.gresur.web;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.gresur.model.Cliente;
 import org.springframework.gresur.service.ClienteService;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,8 +33,10 @@ public class ClienteController {
 		return clienteService.findAll();
 	}
 	
-	@PostMapping
-	public Cliente add(Cliente cliente) throws DataAccessException{
+	@PostMapping("/add/cliente")
+	@PreAuthorize("hasRole('DEPENDIENTE')")
+	@Transactional
+	public Cliente add(@RequestBody @Valid Cliente cliente) throws DataAccessException{
 		return clienteService.save(cliente);
 	}
 	
