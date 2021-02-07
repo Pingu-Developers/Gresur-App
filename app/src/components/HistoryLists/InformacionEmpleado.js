e9485889ecd7a4f5ca830881c88ab7225ddf6844
alesancor1 <alesancor1@alum.us.es>
2021-02-07 18:57:08 +0100

    Revision vista administrador personal

---
+++ app/src/components/HistoryLists/InformacionEmpleado.js
@@ -0,0 +1,87 @@
+import React from 'react';
+
+//MATERIAL UI Stuff
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import DeleteDialogue from '../Dialogs/DeleteDialogue';
+import EditDialogue from '../Dialogs/EditDialogue';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    position: 'relative',
+    display: 'grid',
+    gridTemplateRows: '2fr 10fr',
+    backgroundColor: '#dadada',
+    width: '100%',
+    borderRadius: 20,
+    marginBottom: 20,
+    color: '#7a7a7a',
+    boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.15), 0 6px 20px 0 rgba(0, 0, 0, 0.14)'
+  },
+  nombreElmpleado: {
+    display: 'flex',
+    margin: 0,
+    alignItems: 'center',
+    fontWeight: 'bold',
+    fontSize: 20,
+    paddingLeft: 20,
+    width: '100%'
+  },
+  infoBody: {
+    display: 'grid',
+    padding: 20,
+    gridGap: '10px 20px',
+    alignItems: 'center',
+    gridTemplateRows: '1fr 1fr 1fr 1fr',
+    gridTemplateColumns: '1.2fr 2fr 2fr 3fr',
+    backgroundColor: '#fafafa',
+    borderRadius: 20,
+  },
+  infoContrato: {
+    height: 'calc(100% - 20px)',
+    backgroundColor: 'white',
+    borderRadius: 20,
+    border: '1px solid #bdbdbd',
+    display: 'grid',
+    padding: '10px 20px 10px 20px',
+  },
+  buttonDiv: {
+    position: 'absolute',
+    display: 'inline-flex',
+    right: 0,
+    bottom: 0
+  }
+}));
+
+export default function InformacionEmpleado(props) {
+  const classes = useStyles();
+  return (
+      <div className = {classes.root}>
+          <Typography className = {classes.nombreElmpleado}>{!props.datos.personal ? null:props.datos.personal.name}</Typography>
+          
+          <div className = {classes.infoBody}>
+            <img style = {{gridRow: '1 / span 4', gridColumn: '1', borderRadius: 20}} src={!props.datos.personal ? null:props.datos.personal.image} height = '100%' width = '100%' />      
+            <Typography style = {{gridRow: 1, gridColumn: 2}}><b>Nombre:</b> {!props.datos.personal ? null:props.datos.personal.name} </Typography>
+            <Typography style = {{gridRow: 2, gridColumn: 2}}><b>DNI:</b> {!props.datos.personal ? null:props.datos.personal.nif}</Typography>
+            <Typography style = {{gridRow: 3, gridColumn: 2}}><b>Dirección:</b> {!props.datos.personal ? null:props.datos.personal.direccion}</Typography>
+            {!props.datos.personal.almacen ? null :
+              <Typography style = {{gridRow: 4, gridColumn: 2}}><b>Almacén:</b> {!props.datos.personal ? null:props.datos.personal.almacen.direccion}</Typography>
+            }
+            <Typography style = {{gridRow: 1, gridColumn: 3}}><b>Email:</b> {!props.datos.personal ? null:props.datos.personal.email}</Typography>
+            <Typography style = {{gridRow: 2, gridColumn: 3}}><b>Teléfono:</b> {!props.datos.personal ? null:props.datos.personal.tlf}</Typography>
+            <Typography style = {{gridRow: 3, gridColumn: 3}}><b>NSS:</b> {!props.datos.personal ? null:props.datos.personal.nss}</Typography>
+
+            <div style = {{gridRow: '1 / span 3', gridColumn: 4}} className = {classes.infoContrato}>
+                <Typography><b>Nomina:</b> {!props.datos.personal ? null:props.datos.nomina}€</Typography>
+                <Typography><b>Fecha Inicio:</b> {!props.datos.personal ? null:props.datos.fechaInicio}</Typography>
+                <Typography><b>Fecha Fin:</b> {!props.datos.personal ? null:props.datos.fechaFin}</Typography >
+                <Typography><b>Jornada:</b>  {!props.datos.personal ? null:props.datos.tipoJornada}</Typography>
+            </div>
+          </div>
+          <div className = {classes.buttonDiv}>
+            <EditDialogue edicion={!props.datos ? '':props.datos}/>
+            <DeleteDialogue eliminacion={!props.datos ? '':props.datos.personal}/>
+          </div>
+      </div>
+    );
+}
--- app/src/pages/administradorPersonal.js
+++ app/src/pages/administradorPersonal.js
@@ -5,23 +5,110 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { loadPersonalContrato, clear} from '../redux/actions/dataActions';
 
+//Mui
+import withStyles from '@material-ui/core/styles/withStyles';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
 
 //Components
-import TablaPersonalDesplegable from '../components/Tables/TablaPersonalDesplegable';
+import InformacionEmpleado from '../components/HistoryLists/InformacionEmpleado';
 import PopUpNuevoEmpleado from '../components/Dialogs/PopUpNuevoEmpleado';
 import SnackCallController from '../components/Other/SnackCallController';
 
+// VERTICAL TAB PANEL
+function TabPanel(props) {
+    const { children, value, index, ...other } = props;
 
+    return (
+        <div
+            role="tabpanel"
+            hidden={value !== index}
+            id={`vertical-tabpanel-${index}`}
+            aria-labelledby={`vertical-tab-${index}`}
+            {...other}
+            >
+            {value === index && (
+                <Box p={3}>
+                <Typography>{children}</Typography>
+                </Box>
+            )}
+        </div>
+    );
+}
+  
+TabPanel.propTypes = {
+    children: PropTypes.node,
+    index: PropTypes.any.isRequired,
+    value: PropTypes.any.isRequired,
+};
+
+function a11yProps(index) {
+    return {
+        id: `vertical-tab-${index}`,
+        'aria-controls': `vertical-tabpanel-${index}`,
+    };
+}
+
+//PAGE
+const style = theme => ({
+    root: {
+        flexGrow: 1,
+        backgroundColor: theme.palette.background.paper,
+        display: 'grid',
+        gridTemplateColumns: '3fr 20fr',
+        gridTemplateRows: '30fr',
+        height: 384,
+    },
+    tabs: {
+        borderRight: `1px solid ${theme.palette.divider}`,
+    },
+    tabBtn: {
+        '&:hover':{
+            backgroundColor: 'rgba(0, 188, 212, 0.2)',
+        },
+    }
+})
 export class administradorPersonal extends Component {
     constructor(props){
         super(props);
         this.state = {
+            value:0,
             data:[]
-        }
+        };
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(newValue) {
+        this.setState({
+            value:newValue
+        })
     }
 
     componentDidMount(){
-        this.props.loadPersonalContrato();
+        this.props.loadPersonalContrato("TODOS");
+    }
+
+    componentDidUpdate(prevprops, prevstate) {
+        if(prevstate.value !== this.state.value){
+            switch(this.state.value){
+                case 0:
+                    this.props.loadPersonalContrato("TODOS"); break;
+                case 1:
+                    this.props.loadPersonalContrato("TRANSPORTISTA"); break;
+                case 2:
+                    this.props.loadPersonalContrato("DEPENDIENTE"); break;
+                case 3:
+                    this.props.loadPersonalContrato("ENCARGADO"); break;
+                case 4:
+                    this.props.loadPersonalContrato("ADMINISTRADOR"); break;
+                default:
+                    this.props.loadPersonalContrato("TODOS");
+                    this.setState({value: 0}); break;
+                    
+            }
+        }
     }
 
     componentWillUnmount(){
@@ -36,11 +123,31 @@ export class administradorPersonal extends Component {
                 <div>
                     <PopUpNuevoEmpleado/>
                 </div>
-                          
-                <div>
-                    {data.personal===undefined?null:
-                    <TablaPersonalDesplegable datos = {data.contrato} />  
-                    }
+
+                <div className = {classes.root}>
+                    <Tabs
+                    orientation="vertical"
+                    indicatorColor="secondary"
+                    textColor="secondary"
+                    value={this.state.value}
+                    onChange={(event, newValue)=>{this.handleChange(newValue)}}
+                    aria-label="Vertical tabs example"
+                    className={classes.tabs}
+                    >
+                        <Tab label="Todos" {...a11yProps(0)} className = {classes.tabBtn}/>
+                        <Tab label="Transportistas" {...a11yProps(1)} className = {classes.tabBtn}/>
+                        <Tab label="Dependientes" {...a11yProps(2)} className = {classes.tabBtn}/>
+                        <Tab label="Encargados de almacen" {...a11yProps(3)} className = {classes.tabBtn}/>
+                        <Tab label="Administradores" {...a11yProps(4)} className = {classes.tabBtn}/>
+                    </Tabs>
+
+                    <TabPanel value={this.state.value} index={this.state.value}>
+                        {data.contrato.length === 0 ? <p>No se han podido cargar los datos</p>:
+                            data.contrato.map( (cont) => 
+                                <InformacionEmpleado datos = {cont} />  
+                            )
+                        }
+                    </TabPanel>                    
                 </div>
             </div>
         )
@@ -48,7 +155,6 @@ export class administradorPersonal extends Component {
 }      
 
 administradorPersonal.propTypes = {
-    classes: PropTypes.object.isRequired,
     data: PropTypes.object.isRequired,
     loadPersonalContrato: PropTypes.func.isRequired,
 }
@@ -63,4 +169,4 @@ const mapActionsToProps = {
     clear
 }
 
-export default connect(mapStateToProps, mapActionsToProps)(administradorPersonal)
+export default connect(mapStateToProps, mapActionsToProps)(withStyles(style)(administradorPersonal));
